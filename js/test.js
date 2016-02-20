07654e2cf3c2a8ae3221302fe42f255ddcb32b63
aplescia <anthony.plescia@mail.mcgill.ca>
2016-02-20 14:36:47 -0500

    example function

--- js/test.js
+++ js/test.js
@@ -1,1 +1,13 @@
-var request = require('request')
+
+//Set up request variable
+var request = require('request');
+
+request.post(
+    // 'site url here'
+   // {form : {key: 'value'}},
+   //function (error, response, body) {
+     //  if (!error && response.statusCode == 200){
+       //    console.log(body);
+       //}
+   //}
+);
