b7bc80ba37b2596cabc3d7552d7bdacfff8c1480
aplescia <anthony.plescia@mail.mcgill.ca>
2016-02-20 14:42:43 -0500

    changed url for test function

--- js/test.js
+++ js/test.js
@@ -2,12 +2,8 @@
 //Set up request variable
 var request = require('request');
 
-request.post(
-    // 'site url here'
-   // {form : {key: 'value'}},
-   //function (error, response, body) {
-     //  if (!error && response.statusCode == 200){
-       //    console.log(body);
-       //}
-   //}
-);
+request('http://mybuddy.azurewebsites.net', function (error, response, body) {
+  if (!error && response.statusCode == 200) {
+    console.log(body)
+  }
+})
