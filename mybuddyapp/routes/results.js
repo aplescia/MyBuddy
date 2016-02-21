b081d7e3ee3a8779cedd8fd60fe5b06d903ec510
aplescia <anthony.plescia@mail.mcgill.ca>
2016-02-20 21:52:58 -0500

    ghetto route for results of json

--- mybuddyapp/routes/results.js
+++ mybuddyapp/routes/results.js
@@ -1,4 +1,33 @@
+/*
+Requires
+*/
 var express = require('express');
 var request = require('request');
+var _ = require('underscore');
 
 var router = express.Router();
+
+/*GET results*/
+function getJSON(latitudeCoordinates, longitudeCoordinates) {
+    var tripAdvisorURL = 'http://api.tripadvisor.com/api/partner/2.0/map/' + latitudeCoordinates + ',' + longitudeCoordinates + '/hotels?key=33C9422BFBA246AE8D4A81AD08919253';
+    console.log(tripAdvisorURL);
+    request(tripAdvisorURL  , function(error,response,body){
+        if (!error && response.statusCode == 200){
+            var results = JSON.parse(body);
+            var hotels = _.min(results.data, function(location) {
+                var rankingData = location.ranking_data;
+                if (!rankingData) {
+                    return 999999999
+                } else {
+                    return parseInt(rankingData.ranking);
+                }
+            });
+            console.log(hotels);
+            return hotels;
+        }
+    });
+}
+
+var hotel = getJSON('1','-1');
+
+module.exports = router;
