8dcee94066017b9d7c1d85354d4c9b2abb5111de
LamUong <lam.uong2@mail.mcgill.ca>
2016-02-21 10:08:26 -0500

    update

--- mybuddyapp/public/js/chat.js
+++ mybuddyapp/public/js/chat.js
@@ -2,25 +2,44 @@ window.onload = function() {
  
     var messages = [];
     var socket = io.connect('http://localhost:3000');
+    var name = document.getElementById("NameofPerson");
     var field = document.getElementById("field");
     var sendButton = document.getElementById("send");
     var content = document.getElementById("content");
  
-    socket.on('message', function (data) {
-        if(data.message) {
-            messages.push(data.message);
+    socket.on('message', function (data,first) {
+        if (first){
+            var List = data.message.toString().split(',')
+            for(var i=0; i<List.length; i++) {
+                messages.push(List[i]);
+            }
+            if(data.message) {
             var html = '';
             for(var i=0; i<messages.length; i++) {
                 html += messages[i] + '<br />';
             }
             content.innerHTML = html;
-        } else {
-            console.log("There is a problem:", data);
+            } else {
+                console.log("There is a problem:", data);
+            }
+        }
+        else{
+            if(data.message) {
+                messages.push(data.message);
+            var html = '';
+            for(var i=0; i<messages.length; i++) {
+                html += messages[i] + '<br />';
+            }
+            content.innerHTML = html;
+            } else {
+                console.log("There is a problem:", data);
+            }
         }
     });
     sendButton.onclick = function() {
         var text = field.value;
-        socket.emit('send', { message: text });
+        var textwithname = name.value+": " + text;
+        socket.emit('send', { message: textwithname });
     };
  
 }
