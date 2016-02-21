aa54cdaf7a34e5310172cbd5829ecd29b935ff03
arthurkam <arthur.kam@hotmail.com>
2016-02-20 21:14:37 -0500

    Add location search function

--- mybuddyapp/app.js
+++ mybuddyapp/app.js
@@ -8,6 +8,7 @@ var request = require('request'); //request package
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var geocoder = require('./routes/geocoder')
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/geocoder', geocoder);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
