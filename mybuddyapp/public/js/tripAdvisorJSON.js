98b2876a637c90995225510b61e1f34625802b5f
aplescia <anthony.plescia@mail.mcgill.ca>
2016-02-20 19:22:38 -0500

    added basic function

--- mybuddyapp/public/js/tripAdvisorJSON.js
+++ mybuddyapp/public/js/tripAdvisorJSON.js
@@ -1,9 +1,15 @@
 var request = require('request')
-var latitudeCoordinates = '';
-var longitudeCoordinates = '';
-var tripAdvisorURL = 'http://api.tripadvisor.com/api/partner/2.0/map/' + latitudeCoordinates + longitudeCoordinates + '/hotels?key=33C9422BFBA246AE8D4A81AD08919253';
-request(tripAdvisorURL  , function(error,response,body){
-    if (!error && response.statusCode == 200){
-        console.log(body);
-    }
-})
+var underScore = require('underscore');
+var hotels = [];
+
+
+function getJSON(latitudeCoordinates, longitudeCoordinates) {
+    var tripAdvisorURL = 'http://api.tripadvisor.com/api/partner/2.0/map/' + latitudeCoordinates + longitudeCoordinates + '/hotels?key=33C9422BFBA246AE8D4A81AD08919253';
+    request(tripAdvisorURL  , function(error,response,body){
+        if (!error && response.statusCode == 200){
+            hotels = _.max(body,function(rank){
+                return parseInt(body.ranking_data.ranking);
+            });
+        }
+    });
+}
